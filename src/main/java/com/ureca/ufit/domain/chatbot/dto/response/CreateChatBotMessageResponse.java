23a7f728ae3a9f306efd75149601a270e21542a4
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-06-17 00:02:34 +0900

    fix: messageId를 String으로 수정

--- src/main/java/com/ureca/ufit/domain/chatbot/dto/response/CreateChatBotMessageResponse.java
+++ src/main/java/com/ureca/ufit/domain/chatbot/dto/response/CreateChatBotMessageResponse.java
@@ -1,19 +1,18 @@
 package com.ureca.ufit.domain.chatbot.dto.response;
 
+import java.util.List;
 import java.util.Map;
 
 import com.ureca.ufit.entity.enums.AnswerType;
 
 public record CreateChatBotMessageResponse(
 
-	Long messageId,
-
-	Long userId,
+	String messageId,
 
 	String answer,
 
 	AnswerType answerType,
 
-	Map<String, Map<String, Object>> recommendPlans
+	List<Map<String, Object>> recommendPlans
 ) {
 }
