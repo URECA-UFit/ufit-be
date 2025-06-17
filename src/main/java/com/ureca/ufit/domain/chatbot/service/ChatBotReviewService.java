5d1efaf1a155cd89d6820e1043a601c215fa84b4
권영태 <56019823+dudxo@users.noreply.github.com>
2025-06-17 15:20:03 +0900

    feat: 챗봇 리뷰 API (#82)
    * feat: ChatBotReivew 챗봇메시지ID 필드 추가
    
    * feat: 챗봇 리뷰 중복/금칙어 검증 추가
    
    * test: 챗봇 리뷰 예외 단위 테스트
    
    * style: 코드 포맷팅
    
    * style: 변수명 변경
    
    * style: 주석 제거 및 DTO 필드명 변경에 따른 네이밍 수정
    
    * feat: 챗봇 리뷰 저장 valid 추가 및 리팩토링
    
    * test: 챗봇 리뷰 저장 valid 단위 테스트

--- src/main/java/com/ureca/ufit/domain/chatbot/service/ChatBotReviewService.java
+++ src/main/java/com/ureca/ufit/domain/chatbot/service/ChatBotReviewService.java
@@ -1,8 +1,10 @@
 package com.ureca.ufit.domain.chatbot.service;
 
 import static com.ureca.ufit.domain.chatbot.exception.ChatBotErrorCode.*;
+import static com.ureca.ufit.global.profanity.BanwordFilterPolicy.*;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,8 @@ import com.ureca.ufit.domain.chatbot.dto.response.QuestionSummaryDto;
 import com.ureca.ufit.domain.chatbot.repository.ChatBotReviewRepository;
 import com.ureca.ufit.entity.ChatBotReview;
 import com.ureca.ufit.global.exception.RestApiException;
+import com.ureca.ufit.global.profanity.BanwordFilterPolicy;
+import com.ureca.ufit.global.profanity.ProfanityService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,27 +30,54 @@ public class ChatBotReviewService {
 
 	@Value("${llm.base-url}")
 	private String llmBaseUrl;
+	private Set<BanwordFilterPolicy> policies = Set.of(NUMBERS, WHITESPACES);
 
 	private final RestTemplate restTemplate;
 	private final ChatBotReviewRepository chatBotReviewRepository;
+	private final ProfanityService profanityService;
+	private final ChatBotMessageService chatBotMessageService;
+	private final ChatRoomService chatRoomService;
 
 	public CreateChatBotReviewResponse createChatBotReview(CreateChatBotReviewRequest request) {
+		validateCreateChatBotReviewRequest(request);
 
+		QuestionSummaryDto questionSummaryDto = requestUserQuerySummary(request);
+
+		createAndSaveChatBotReview(request, questionSummaryDto);
+
+		return ChatBotReviewMapper.toCreateChatBotReviewResponse();
+	}
+
+	private void validateCreateChatBotReviewRequest(CreateChatBotReviewRequest request) {
+		chatRoomService.getValidatedChatRoom(request.chatRoomId());
+		chatBotMessageService.validateMessageBelongsToChatRoom(request.recommendationMessageId(), request.chatRoomId());
+		validateDuplicatedChatBotReview(request.recommendationMessageId());
+		validateProfanity(request.content());
+	}
+
+	private void validateDuplicatedChatBotReview(String recommendationMessageId) {
+		if (chatBotReviewRepository.existsByChatBotMessageId(recommendationMessageId)) {
+			throw new RestApiException(CHAT_BOT_REVIEW_DUPLICATED);
+		}
+	}
+
+	private void validateProfanity(String content) {
+		if (profanityService.containsBannedWord(content, policies)) {
+			throw new RestApiException(CONTENT_RESTRICTED_WORD);
+		}
+	}
+
+	private QuestionSummaryDto requestUserQuerySummary(CreateChatBotReviewRequest request) {
 		final String url = String.format("%s/api/chats/review/%d", llmBaseUrl, request.chatRoomId());
 
 		CreateUserQuerySummaryRequest chatReviewSummaryRequest = new CreateUserQuerySummaryRequest(
-			request.recommendation_message_id());
+			request.recommendationMessageId());
 
 		QuestionSummaryDto questionSummaryDto = restTemplate.postForObject(url, chatReviewSummaryRequest,
 			QuestionSummaryDto.class);
 
 		validateSummary(questionSummaryDto);
-
-		ChatBotReview chatBotReview = ChatBotReviewMapper.toChatBotReview(request, questionSummaryDto);
-
-		chatBotReviewRepository.save(chatBotReview);
-
-		return ChatBotReviewMapper.toCreateChatBotReviewResponse();
+		return questionSummaryDto;
 	}
 
 	private void validateSummary(QuestionSummaryDto questionSummaryDto) {
@@ -54,5 +85,11 @@ public class ChatBotReviewService {
 			.map(QuestionSummaryDto::summary)
 			.filter(StringUtils::hasText)
 			.orElseThrow(() -> new RestApiException(LLM_SUMMARY_FAIL));
+	}
+
+	private void createAndSaveChatBotReview(CreateChatBotReviewRequest request, QuestionSummaryDto questionSummaryDto) {
+		ChatBotReview chatBotReview = ChatBotReviewMapper.toChatBotReview(request, questionSummaryDto);
+
+		chatBotReviewRepository.save(chatBotReview);
 	}
 }
