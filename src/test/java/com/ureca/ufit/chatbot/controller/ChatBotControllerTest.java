901367eb0d1a7266087893ebeded970030dde02c
heejjinkim <hjhj12@dgu.ac.kr>
2025-06-18 11:25:20 +0900

    test: 채팅방 과거 메시지 조회 api Controller 테스트

--- src/test/java/com/ureca/ufit/chatbot/controller/ChatBotControllerTest.java
+++ src/test/java/com/ureca/ufit/chatbot/controller/ChatBotControllerTest.java
@@ -6,20 +6,28 @@ import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
 
 import com.ureca.ufit.common.support.ApiSupport;
+import com.ureca.ufit.domain.chatbot.dto.response.ChatMessageDto;
 import com.ureca.ufit.domain.chatbot.dto.response.ChatRoomCreateResponse;
 import com.ureca.ufit.domain.chatbot.service.ChatBotMessageService;
 import com.ureca.ufit.domain.chatbot.service.ChatRoomService;
+import com.ureca.ufit.global.dto.CursorPageResponse;
 
 class ChatBotControllerTest extends ApiSupport {
 
 	@MockBean
 	ChatRoomService chatRoomService;
 
+	@MockBean
+	ChatBotMessageService chatBotMessageService;
 
 	@DisplayName("이미 존재하는 채팅방이 있으면 ID만 반환한다.")
 	@Test
@@ -57,4 +65,26 @@ class ChatBotControllerTest extends ApiSupport {
 			.andExpect(jsonPath("$.isAnonymous").value(true));
 	}
 
+	@DisplayName("특정 채팅방의 메시지를 Cursor 방식으로 조회한다.")
+	@Test
+	void getMessagesWithCursorSuccess() throws Exception {
+		// given
+		Long chatRoomId = 3L;
+		String lastMsgId = "665f3af6d6795e4f5e8e5a12";
+
+		ChatMessageDto dto = new ChatMessageDto(lastMsgId, "안녕", true, Collections.emptyList());
+		CursorPageResponse<ChatMessageDto> stub = new CursorPageResponse<>(List.of(dto), null, false);
+
+		given(chatBotMessageService.getChatMessages(eq(chatRoomId), any(Pageable.class), eq(lastMsgId)))
+			.willReturn(stub);
+
+		// when // then
+		mockMvc.perform(get("/api/chats/{chatroomId}", chatRoomId)
+				.param("lastMessageId", lastMsgId)
+				.param("size", "10")
+				.header("Authorization", accessTokenOfUser))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.item[0].messageId").value(dto.messageId()))
+			.andExpect(jsonPath("$.hasNext").value(false));
+	}
 }
