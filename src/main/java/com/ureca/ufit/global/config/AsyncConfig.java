4e709b3a0ca94600f31aee2f6851d0c07c7ca4e4
권영태 <56019823+dudxo@users.noreply.github.com>
2025-06-22 02:38:09 +0900

    chore: 배포 (#120)
    * chore: 모니터링 시스템 (#115)
    
    * chore: prometheus, actuator 의존 추가
    
    * chore: 모니터링 시스템 관련 스크립트 추가
    
    * feat: JwtFilter WHITELIST actuator 관련 경로 추가
    
    * fix: 모니터링 시스템 로컬 테스트 관련 파일 제거
    
    * feat: RestTemplate + Async 추가 (#119)
    
    * feat: Async Config 추가
    
    * feat: RestTemplate + Async 추가

---
+++ src/main/java/com/ureca/ufit/global/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.ureca.ufit.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
